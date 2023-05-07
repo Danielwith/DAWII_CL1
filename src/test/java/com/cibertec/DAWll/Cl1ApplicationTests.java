cb27a34fd7e7ef0d11e6786dec5ed8f8c77efc11
Daniel Orosco <daniel.orosco.sc2@hotmail.com>
2023-05-06 21:56:05 -0500

    Initial commit

---
+++ src/test/java/com/cibertec/DAWll/Cl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAWll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
